6d9c34d740d0b24fd95d79fce013d5391bf4db7f
Scott Fox <scottfox5@gmail.com>
2017-04-02 13:23:06 -0500

    heroku deploy

--- server/routes/tasks.js
+++ server/routes/tasks.js
@@ -1,66 +1,32 @@
 var express = require("express");
 var router = express.Router();
+
 var pg = require("pg");
+
 var config = { database: "to_do_list_db" };
-// process.env.DATABASE_URL
+
 var pool = new pg.Pool(config);
-
-// var PGUSER = 'xacxunfinnebxe'
-// var PGDATABSE ='dab1q8970c41lh'
-// var PGPASSWORD = '0b44df3e43dea3d305eaad219b1ed4d660995b8a79b066fff40ec45dd755c935'
-// var PGHOST = 'ec2-107-22-244-62.compute-1.amazonaws.com'
-//
-// var pool = new pg.Pool({
-//   user: process.env.PGUSER, //env var: PGUSER
-//   database: process.env.PGDATABASE, //env var: process.env.PGDATABASE
-//   password: process.env.PGPASSWORD, //env var: PGPASSWORD
-//   host: process.env.PGHOST, // PGHOST Server hosting the postgres database
-//   port: 5432, //env var: PGPORT
-//   max: 10, // max number of clients in the pool
-//   idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
-//
-// });
-
-
-// // If we are running on Heroku, use the remote database (with SSL)
-// if(process.env.DATABASE_URL != undefined) {
-//     connectionString = process.env.DATABASE_URL + "?ssl=true";
-// } else {
-//     // running locally, use our local database instead
-//     connectionString = 'postgres://localhost:5432/to_do_list_db';
-// }
 
 router.get("/", function(req, res) {
 
-  pg.defaults.ssl = true;
-  pg.connect(process.env.DATABASE_URL, function(err, client) {
-    if (err) throw err;
-    console.log('Connected to postgres! Getting schemas...');
-
-    client
-      .query('SELECT * FROM tasks;')
-      .on('row', function(row) {
-        console.log(JSON.stringify(row));
+  pool.connect(function(err, client, done) {
+    if (err) {
+      console.log("Error connecting to DB", err);
+      res.sendStatus(500);
+      done();
+    } else {
+      client.query("SELECT * FROM tasks ORDER BY complete ASC", function(err, result) {
+        done();
+        if (err) {
+          console.log("Error querying DB", err);
+          res.sendStatus(500);
+        } else {
+          console.log("Got info from DB", result.rows);
+          res.send(result.rows);
+        }
       });
+    }
   });
-  // pool.connect(function(err, client, done) {
-  //   if (err) {
-  //     console.log("Error connecting to DB", err);
-  //     res.sendStatus(500);
-  //     done();
-  //   } else {
-  //     client.query("SELECT * FROM tasks ORDER BY complete ASC", function(err, result) {
-  //       done();
-  //       if (err) {
-  //         console.log("Error querying DB", err);
-  //         res.sendStatus(500);
-  //       } else {
-  //         console.log("Got info from DB", result.rows);
-  //         res.send(result.rows);
-  //       }
-  //     });
-  //   }
-  // });
 });
 
 router.post("/", function(req, res) {

ed9258dcebef7c0f2a185609a92822202341c8a5
Scott Fox <scottfox5@gmail.com>
2017-04-01 23:23:28 -0500

    update sample database queries

--- data/database.sql
+++ data/database.sql
@@ -10,7 +10,7 @@ CREATE TABLE tasks (
 );
 
 INSERT INTO tasks (task,complete,notes,updated,list_id)
-VALUES ('get groceries',false,'apples bananas milk eggs bread','2017-02-23 23:00:00', 1);
+VALUES ('example1',false,'example details 1','2017-02-23 23:00:00', 1);
 
 INSERT INTO tasks (task,complete,notes,updated,list_id)
-VALUES ('excercise',false,'intervals kettle bells planks burpees pull-ups','2017-01-20 06:30:00', 1);
+VALUES ('example2',false,'example details 2','2017-01-20 06:30:00', 1);

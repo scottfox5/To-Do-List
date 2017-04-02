e58c0b0b43378599136267650c15a1fd3301f16c
Scott Fox <scottfox5@gmail.com>
2017-04-02 11:36:32 -0500

    heroku deploy

--- server/routes/tasks.js
+++ server/routes/tasks.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var router = express.Router();
 var pg = require("pg");
-var config = { database: process.env.DATABASE_URL };
+var config = { database: "to_do_list_db" };
 // process.env.DATABASE_URL
 var pool = new pg.Pool(config);
 

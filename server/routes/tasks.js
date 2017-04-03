c2be534c66631ee9d7d409fb73db5be7b0186f92
Scott Fox <scottfox5@gmail.com>
2017-04-02 20:19:46 -0500

    heroku deploy

--- server/routes/tasks.js
+++ server/routes/tasks.js
@@ -7,35 +7,36 @@ var pool = new pg.Pool(config);
 
 router.get("/", function(req, res) {
   // trying to connect SQL database for heroku deployment
-  pg.defaults.ssl = true;
-  pg.connect(process.env.DATABASE_URL, function(err, client) {
-    if (err) throw err;
-    console.log('Connected to postgres! Getting schemas...');
-
-    client.query("SELECT * FROM tasks ORDER BY complete ASC")
-      .on('row', function(row) {
-        console.log('Row:', JSON.stringify(row));
-      });
-  });
-
-  // pool.connect(function(err, client, done) {
-  //   if (err) {
-  //     console.log("Error connecting to DB", err);
-  //     res.sendStatus(500);
-  //     done();
-  //   } else {
-  //     client.query("SELECT * FROM tasks ORDER BY complete ASC", function(err, result) {
-  //       done();
-  //       if (err) {
-  //         console.log("Error querying DB", err);
-  //         res.sendStatus(500);
-  //       } else {
-  //         console.log("Got info from DB", result.rows);
-  //         res.send(result.rows);
-  //       }
+  // pg.defaults.ssl = true;
+  // pg.connect(process.env.DATABASE_URL, function(err, client) {
+  //   if (err) throw err;
+  //   console.log('Connected to postgres! Getting schemas...');
+  //
+  //   client.query("SELECT * FROM tasks ORDER BY complete ASC")
+  //     .on('row', function(row) {
+  //       console.log('Row:', JSON.stringify(row));
   //     });
-  //   }
   // });
+
+  pool.connect(function(err, client, done) {
+    if (err) {
+      console.log("Error connecting to DB", err);
+      res.sendStatus(500);
+      done();
+    } else {
+      console.log('Connected to postgres! Getting schemas...');
+      client.query("SELECT * FROM tasks ORDER BY complete ASC", function(err, result) {
+        done();
+        if (err) {
+          console.log("Error querying DB", err);
+          res.sendStatus(500);
+        } else {
+          console.log("Got info from DB", result.rows);
+          res.send(result.rows);
+        }
+      });
+    }
+  });
 });
 
 router.post("/", function(req, res) {
